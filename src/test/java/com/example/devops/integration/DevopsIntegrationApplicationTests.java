c74620d0d43dcff9bcf16bc27aedd32f8fe19ca7
satya012025 <sivalingamsatya@gmail.com>
2025-04-05 22:53:06 +0530

    first commit

---
+++ src/test/java/com/example/devops/integration/DevopsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
